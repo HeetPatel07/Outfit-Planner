bde7f43f55f88466d830bedb311ca34b4f49b83f
Max Rollwage <maxr@dev.bredex.de>
2023-11-11 14:57:43 -0600

    Added functionality for the sidebar, so switching between different categories is possible. Also added text and a background color for the buttons when hovering over those.

--- scripts/items-script.js
+++ scripts/items-script.js
@@ -1,13 +1,27 @@
-var categories = document.querySelectorAll('.category');
-categories.forEach(function(category) {
-    category.addEventListener('click', function() {
-        if (this.classList.contains('selected')) {
-            this.classList.remove('selected');
-        } else {
+window.addEventListener('load', function() {
+    var categories = document.querySelectorAll('.category');
+    var tables = document.querySelectorAll('.table');
+
+    // Default selected category is tops([1])
+    categories[1].classList.add('selected');
+
+    categories.forEach(function(category) {
+        category.addEventListener('click', function() {
+            console.log('Category clicked');
             categories.forEach(function(c) {
                 c.classList.remove('selected');
             });
             this.classList.add('selected');
-        }
+
+            // Hide all tables
+            tables.forEach(function(table) {
+                table.style.display = 'none';
+            });
+
+            // Show the table corresponding to the selected category
+            var selectedCategory = this.getAttribute('data-category');
+            var selectedTable = document.querySelector('.' + selectedCategory);
+            selectedTable.style.display = 'block';
+        });
     });
 });
