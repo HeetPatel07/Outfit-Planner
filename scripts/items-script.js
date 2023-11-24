ab61cf7b200442764ca6c2d519e91f31af92e2af
Max Rollwage <maxr@dev.bredex.de>
2023-11-24 17:10:54 -0600

    Implemented adding functionality for the "Items"-section that is based on JavaScript objects and arrays that act as a datastructure for storing the different items.

--- scripts/items-script.js
+++ scripts/items-script.js
@@ -1,8 +1,6 @@
 window.addEventListener('load', function() {
     var categories = document.querySelectorAll('.category');
     var tables = document.querySelectorAll('.table');
-    var currentIndex = 0; // Index of the currently displayed page
-    var pages = document.querySelectorAll('.page');
 
     // Default selected category is tops([1])
     categories[1].classList.add('selected');
@@ -25,29 +23,5 @@ window.addEventListener('load', function() {
             var selectedTable = document.querySelector('.' + selectedCategory);
             selectedTable.style.display = 'block';
         });
-    });
-
-    // Add event listener to the next button
-    document.getElementById('next').addEventListener('click', function() {
-        // Hide the current page
-        pages[currentIndex].style.display = 'none';
-        // Show the next page
-        currentIndex++;
-        if (currentIndex >= pages.length) { // If we're at the last page, go back to the first one
-            currentIndex = 0;
-        }
-        pages[currentIndex].style.display = 'block';
-    });
-
-    // Add event listener to the previous button
-    document.getElementById('prev').addEventListener('click', function() {
-        // Hide the current page
-        pages[currentIndex].style.display = 'none';
-        // Show the previous page
-        currentIndex--;
-        if (currentIndex < 0) { // If we're at the first page, go to the last one
-            currentIndex = pages.length - 1;
-        }
-        pages[currentIndex].style.display = 'block';
     });
 });
