b25a9b94ab3e20594706eb063e9cea84e96da60c
gerrag <gerrag@myumanitoba.ca>
2023-11-30 21:08:07 -0600

    Added home page outfit modal

---
+++ scripts/home.js
@@ -0,0 +1,9 @@
+const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+const temps = ['-12°C', '-11°C', '-13°C', '-9°C', '-4°C', '-16°C', '-5°C', ];
+const outfits = [];
+
+
+
+function dayClicked(day, date, outfit){
+    openModal("homeDayModal");
+}
